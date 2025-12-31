081444794ad868f67a525b289cc950d3b8c293ed
github <dochkouern@gmail.com>
2025-12-31 08:20:16 +0700

    Initial Commit

---
+++ Rcc_Project/Rcc_ProjectApp.swift
@@ -0,0 +1,32 @@
+//
+//  Rcc_ProjectApp.swift
+//  Rcc_Project
+//
+//  Created by HRD on 12/31/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Rcc_ProjectApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
